661e6c90fcb5b7a18c0b2c9b0247b1c91dc6dc4d
tribbianiwang <1130455004@qq.com>
2021-03-23 16:28:27 +0800

    作用域实现

--- app/src/main/java/com/wl/androidlearning/hilt/NetworkModule.kt
+++ app/src/main/java/com/wl/androidlearning/hilt/NetworkModule.kt
@@ -5,21 +5,24 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.components.ApplicationComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
 
 @Module
-@InstallIn(ActivityComponent::class)
+@InstallIn(ApplicationComponent::class)
 class NetworkModule {
-
+    @Singleton
     @Provides
     fun provideOkhttpClients():OkHttpClient{
         return  OkHttpClient.Builder().connectTimeout(20,TimeUnit.SECONDS).readTimeout(20,TimeUnit.SECONDS)
             .writeTimeout(20,TimeUnit.SECONDS).build()
     }
-
+    @Singleton
     @Provides
     fun provideRetrofit(okHttpClient: OkHttpClient):Retrofit{
 
